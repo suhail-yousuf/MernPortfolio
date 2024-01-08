8ba6b2f444ad6518e9eccc680d9f3cecc710e178
Suhail Yousuf <suhaily312@gmail.com>
2024-01-08 13:57:40 +0500

    added and exp

--- src/Pages/Workexp/Workexp.js
+++ src/Pages/Workexp/Workexp.js
@@ -15,7 +15,7 @@ const Workexp = () => {
     className="vertical-timeline-element--work"
     contentStyle={{ background: 'white', color: '#1e1e2c' }}
     contentArrowStyle={{ borderRight: '7px solid  white' }}
-    date="20221 - present"
+    date="01-05-2021  -  01-08-2021 "
     iconStyle={{ background: 'rgb(33, 150, 243)', color: '#1e1e2c' }}
     icon={<MdOutlineWorkHistory />}>
     <h3 className="vertical-timeline-element-title">Junior Developer</h3>
@@ -36,42 +36,20 @@ backend.
     className="vertical-timeline-element--work"
     contentStyle={{ background: 'white', color: '#1e1e2c' }}
     contentArrowStyle={{ borderRight: '7px solid  white' }}
-    date="20221 - present"
+    date="2023 - present"
     iconStyle={{ background: 'rgb(33, 150, 243)', color: '#1e1e2c' }}
     icon={<MdOutlineWorkHistory />}>
     <h3 className="vertical-timeline-element-title">Junior Developer</h3>
     <h4 className="vertical-timeline-element-subtitle">Rinor,Skardu</h4>
     <p>
-    3-month intensive internship was
-an eye-opening experience for
-me. During these 3 months at
-Rinor, I learned a lot about the
-JavaScript ecosystem from
-vanilla JavaScript to React.js for
-the Frontend and Node.js,
-Express.js, and MangoDB for the
-backend.
-    </p>
-  </VerticalTimelineElement>
-  <VerticalTimelineElement
-    className="vertical-timeline-element--work"
-    contentStyle={{ background: 'white', color: '#1e1e2c' }}
-    contentArrowStyle={{ borderRight: '7px solid  white' }}
-    date="20221 - present"
-    iconStyle={{ background: 'rgb(33, 150, 243)', color: '#1e1e2c' }}
-    icon={<MdOutlineWorkHistory />}>
-    <h3 className="vertical-timeline-element-title">Junior Developer</h3>
-    <h4 className="vertical-timeline-element-subtitle">Rinor,Skardu</h4>
-    <p>
-    3-month intensive internship was
-an eye-opening experience for
-me. During these 3 months at
-Rinor, I learned a lot about the
-JavaScript ecosystem from
-vanilla JavaScript to React.js for
-the Frontend and Node.js,
-Express.js, and MangoDB for the
-backend.
+    I have been actively engaged in the development of an ERP (Enterprise Resource Planning)
+     system tailored for a Destination Management Company (DMC). My primary responsibilities 
+     involve crafting robust React components to enhance the user interface, ensuring a seamless
+      and intuitive experience for end-users. Additionally, I am actively involved in building and 
+      optimizing the RESTful API that serves as the backbone for data communication and integration 
+      within the ERP system. This dynamic project allows me to leverage my skills in both front-end
+       and back-end development, contributing to the overall efficiency and functionality of the DMC's 
+       enterprise resource planning infrastructure.
     </p>
   </VerticalTimelineElement>
     </VerticalTimeline>
